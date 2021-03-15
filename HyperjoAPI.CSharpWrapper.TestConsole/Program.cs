6e4a94f9843e4a4fe9f4013db63885148eb896d1
Paranoia <turbo@monerobillonaire.ca>
2021-03-15 09:58:10 +0100

    improved examples

--- HyperjoAPI.CSharpWrapper.TestConsole/Program.cs
+++ HyperjoAPI.CSharpWrapper.TestConsole/Program.cs
@@ -6,9 +6,27 @@ namespace HyperjoAPI.CSharpWrapper.TestConsole
     {
         static void Main(string[] args)
         {
+            //Usage and examples
+            
             HyperjoAPIWrapper hyperjoApi = new HyperjoAPIWrapper();
 
-            var s = hyperjoApi.GetFarming();
+            var globalStatistics = hyperjoApi.GetGlobalStatistics();
+            var medicPlaytime = globalStatistics.Playtime.Medic;
+            var allMoney = globalStatistics.Money;
+
+            var globalGangStatistics = hyperjoApi.GetGlobalGangStatistics();
+            var allGangMembers = globalGangStatistics.GangMembers;
+            var gangCount = globalGangStatistics.Gangs;
+
+            var onlineStatistics = hyperjoApi.GetOnline();
+            var onlinePlayers = onlineStatistics.OnlinePlayers;
+
+            var farmingStatistics = hyperjoApi.GetFarming();
+            foreach (var farmingStatistic in farmingStatistics.Farming)
+            {
+                var name = farmingStatistic.ItemName;
+                var currentPrice = farmingStatistic.CurrentPrice;
+            }
             
             Console.ReadLine();
         }
